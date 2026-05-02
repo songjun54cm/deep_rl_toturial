9d4739e34f03544f27459faec345be4fe79a1d16
JunSong <songjun54cm@gmail.com>
2026-05-02 09:00:17 +0800

    update

--- unit1/unit1.py
+++ unit1/unit1.py
@@ -2,3 +2,7 @@ from pyvirtualdisplay.display import Display
 
 virtual_display = Display(visible=False, size=(1400, 900))
 virtual_display.start()
+
+from stable_baselines3.common.env_util import make_vec_env
+
+env = make_vec_env("LunarLander-v2", n_envs=16)
