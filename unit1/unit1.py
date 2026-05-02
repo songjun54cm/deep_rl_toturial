8001d2047c90d6d8058e636cfc9275e0947b2536
JunSong <songjun54cm@gmail.com>
2026-05-02 10:00:15 +0800

    update

--- unit1/unit1.py
+++ unit1/unit1.py
@@ -3,6 +3,29 @@ from pyvirtualdisplay.display import Display
 virtual_display = Display(visible=False, size=(1400, 900))
 virtual_display.start()
 
+from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
 env = make_vec_env("LunarLander-v2", n_envs=16)
+
+# We added some parameters to accelerate the training
+model = PPO(
+    policy="MlpPolicy",
+    env=env,
+    n_steps=1024,
+    batch_size=64,
+    n_epochs=4,
+    gamma=0.999,
+    gae_lambda=0.98,
+    ent_coef=0.01,
+    verbose=1,
+)
+
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
+
+eval_env = Monitor(gym.make("LunarLander-v3", render_mode="rgb_array"))
+mean_reward, std_reward = evaluate_policy(
+    model, eval_env, n_eval_episodes=10, deterministic=True
+)
+print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
